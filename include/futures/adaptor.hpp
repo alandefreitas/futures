0710418fccf600a42248398213ed6d3f4bf82783
alandefreitas <alandefreitas@gmail.com>
2022-10-02 02:10:29 -0300

    feat(modules): main headers for modules

---
+++ include/futures/adaptor.hpp
@@ -0,0 +1,17 @@
+//
+// Copyright (c) 2021 alandefreitas (alandefreitas@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+//
+
+#ifndef FUTURES_ADAPTOR_HPP
+#define FUTURES_ADAPTOR_HPP
+
+#include <futures/adaptor/make_ready_future.hpp>
+#include <futures/adaptor/then.hpp>
+#include <futures/adaptor/when_all.hpp>
+#include <futures/adaptor/when_any.hpp>
+#include <futures/adaptor/when_any_result.hpp>
+
+#endif // FUTURES_ADAPTOR_HPP
---
+++ include/futures/executor.hpp
@@ -0,0 +1,16 @@
+//
+// Copyright (c) 2021 alandefreitas (alandefreitas@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+//
+
+#ifndef FUTURES_EXECUTOR_HPP
+#define FUTURES_EXECUTOR_HPP
+
+#include <futures/executor/default_executor.hpp>
+#include <futures/executor/inline_executor.hpp>
+#include <futures/executor/is_executor.hpp>
+#include <futures/executor/new_thread_executor.hpp>
+
+#endif // FUTURES_EXECUTOR_HPP
