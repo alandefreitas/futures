8e7fac63663f485ce8fa7fadcdcbdafb6e1f9c41
alandefreitas <alandefreitas@gmail.com>
2022-10-27 23:11:40 -0300

    build(dev-mode): clang flame graphs

--- cmake/futures/dev-options.cmake
+++ cmake/futures/dev-options.cmake
@@ -29,7 +29,6 @@ option(FUTURES_BUILD_LINT "Enable linting" OFF)
 option(FUTURES_BUILD_LINTER "Build C++ project linter" ON)
 option(FUTURES_ALWAYS_LINT "Run the linter before running unit tests" ON)
 
-
 #######################################################
 ### How to build                                    ###
 #######################################################
@@ -38,6 +37,7 @@ option(FUTURES_WARNINGS_AS_ERRORS "Treat warnings as errors." ON)
 option(FUTURES_SANITIZERS "Build with sanitizers." ${DEBUG_MODE})
 option(FUTURES_CATCH2_REPORTER "Reporter Catch2 should use when invoked from ctest." console)
 option(FUTURES_TESTS_SMALL_POOL "Run tests with a default thread pool of size 1." OFF)
+option(FUTURES_TIME_TRACE "Enable clang time-trace." ON)
 
 #######################################################
 ### How to build                                    ###
@@ -51,24 +51,31 @@ option(FUTURES_BUILD_WITH_UTF8 "Accept utf-8 in MSVC by default." ON)
 if (MASTER_PROJECT)
     message("Setting global options")
 
-    # This whole project is for coverage
-    if (FUTURES_BUILD_COVERAGE_REPORT)
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
-        set(CMAKE_C_FLAGS "${DCMAKE_C_FLAGS} --coverage")
+    if (GCC OR CLANG)
+        # This whole project is for coverage
+        if (FUTURES_BUILD_COVERAGE_REPORT AND NOT MSVC)
+            add_compile_options(--coverage)
+        endif ()
+
+        # Time trace in clang
+        if (FUTURES_TIME_TRACE AND CLANG AND CLANG_VERSION_MAJOR GREATER_EQUAL 9)
+            add_compile_options(-ftime-trace)
+        endif ()
+    endif()
+
+    if (MSVC)
+        # Allow exceptions in MSVC
+        if (FUTURES_BUILD_WITH_EXCEPTIONS)
+        add_compile_options(/EHsc)
+        endif ()
+        # Allow utf-8 in MSVC
+        if (FUTURES_BUILD_WITH_UTF8 AND MSVC)
+            add_compile_options(/utf-8)
+        endif ()
     endif ()
 
     # Maybe add sanitizers to all targets
     if (FUTURES_SANITIZERS AND NOT EMSCRIPTEN)
         add_sanitizers()
-    endif ()
-
-    # Allow exceptions in MSVC
-    if (MSVC AND FUTURES_BUILD_WITH_EXCEPTIONS)
-        add_compile_options(/EHsc)
-    endif ()
-
-    # Allow utf-8 in MSVC
-    if (FUTURES_BUILD_WITH_UTF8 AND MSVC)
-        set(CMAKE_CXX_FLAGS "/utf-8")
     endif ()
 endif ()
